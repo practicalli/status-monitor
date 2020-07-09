6959e9e6b3ddf4c01bfbcec01fe042ffcc8c56e2
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-07-09 12:26:34 +0100

    New project: status monitor service
    Created with Clojure Tools, clj-new and the app template
    
    ```
    clojure -A:new app practicalli/status-monitor-service
    ```

---
+++ test/practicalli/status_monitor_service_test.clj
@@ -0,0 +1,7 @@
+(ns practicalli.status-monitor-service-test
+  (:require [clojure.test :refer :all]
+            [practicalli.status-monitor-service :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
