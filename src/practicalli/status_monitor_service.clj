70d9fad3a1ed80379618cca43b7ae3518f4c60ef
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-07-09 14:41:00 +0100

    Application server and default route
    Use http-kit to start and stop an application server instance, managing the
    instance state in a Clojure atom.
    
    Use compojure defroutes to define a default route, with a default response.

--- src/practicalli/status_monitor_service.clj
+++ src/practicalli/status_monitor_service.clj
@@ -1,7 +1,61 @@
 (ns practicalli.status-monitor-service
-  (:gen-class))
+  (:gen-class)
+  (:require [compojure.core :refer [defroutes GET]]
+            [org.httpkit.server :as app-server]))
+
+
+;; Application routing
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defroutes status-monitor
+  (GET "/" [] {:status 200 :body "Status Monitor Dashboard"}))
+
+
+;; System
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Running state of the application server
+(defonce app-server-instance (atom nil))
+
+(defn stop-app-server
+  "Gracefully shutdown the server, waiting 100ms"
+  []
+  (when-not (nil? @app-server-instance)
+    (@app-server-instance :timeout 100)
+    (reset! app-server-instance nil)
+    (println "INFO: Application server stopped")))
+
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  "Start the application server and run the application"
+  [& {:keys [port]
+      :or   {port 8888}}]
+  (println "INFO: Starting server on port: " port)
+
+  (reset! app-server-instance
+          (app-server/run-server #'status-monitor {:port port})))
+
+
+(defn restart-app-server
+  "Convenience function to stop and start the application server"
+  []
+  (stop-app-server)
+  (-main))
+
+
+
+;; REPL Driven Development - useful function calls
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(comment
+
+  ;; start application
+  (-main)
+
+  ;; stop application
+  (stop-app-server)
+
+  ;; restart application
+  (restart-app-server)
+
+  )
