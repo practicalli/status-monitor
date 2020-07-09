0618e6603dc037f16fce87c7ba55486f19ea577a
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-07-09 21:23:15 +0100

    Ring mock: add alias and update unit tests

--- test/practicalli/status_monitor_service_test.clj
+++ test/practicalli/status_monitor_service_test.clj
@@ -1,7 +1,11 @@
 (ns practicalli.status-monitor-service-test
-  (:require [clojure.test :refer :all]
-            [practicalli.status-monitor-service :refer :all]))
+  (:require [clojure.test :refer [deftest is testing]]
+            [ring.mock.request :as  mock]
+            [practicalli.status-monitor-service :as SUT]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest dashboard-test
+  (testing "Testing elements on the dashboard"
+    (is (= (SUT/dashboard (mock/request :get "/"))
+           {:status  200
+            :body    "Status Monitor Dashboard"
+            :headers {}}))))
