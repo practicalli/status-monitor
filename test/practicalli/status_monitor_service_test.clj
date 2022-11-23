3d0670326a294c64257cb53f9c17fb2bc6ed43ad
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2022-11-23 13:15:27 +0000

    test: update alias to namespace name

--- test/practicalli/status_monitor_service_test.clj
+++ test/practicalli/status_monitor_service_test.clj
@@ -1,11 +1,11 @@
 (ns practicalli.status-monitor-service-test
   (:require [clojure.test :refer [deftest is testing]]
             [ring.mock.request :as  mock]
-            [practicalli.status-monitor-service :as SUT]))
+            [practicalli.status-monitor-service :as status-monitor]))
 
 (deftest dashboard-test
   (testing "Testing elements on the dashboard"
-    (is (= (SUT/dashboard (mock/request :get "/"))
+    (is (= (status-monitor/dashboard (mock/request :get "/"))
            {:status  200
             :body    "Status Monitor Dashboard via CircleCI and Heroku"
             :headers {}}))))
